9cdb6eb3b8fc2c71811a118c1895cbc47d420ecd
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-03-06 22:51:56 -0300

    Update cronometro.c

--- algoritmos/cronometro.c
+++ algoritmos/cronometro.c
@@ -6,22 +6,22 @@
 int main() {
 	setlocale(LC_ALL, "Portuguese");
 	
-    int horas = 0, minutos = 0, segundos = 0;
+	int horas = 0, minutos = 0, segundos = 0;
     
-    while(1) {
+	while(1) {
 		system("clear");
 		segundos++;
 		if(segundos == 60) {
-		    segundos = 0;
-		    minutos++;
-		    if(minutos == 60) {
-		        minutos = 0;
-		        horas++;
-		    }
+			segundos = 0;
+			minutos++;
+			if(minutos == 60) {
+				minutos = 0;
+				horas++;
+			}
 		}
 		printf("%.2d : %.2d : %.2d \n", horas, minutos, segundos);
 		sleep(1000);
 	}
-    
-    return 0;
+	
+	return 0;
 }
