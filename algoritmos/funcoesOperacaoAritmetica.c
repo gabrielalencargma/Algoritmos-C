bdfcd9bad7a741791d5315de7fa855dd272d0258
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-03-11 22:45:44 -0300

    Create funcoesOperacaoAritmetica.c

---
+++ algoritmos/funcoesOperacaoAritmetica.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int escolha;
+float n1, n2, resultado;
+
+float soma(float n1, float n2) {
+	return n1 + n2;
+}
+
+float subtracao(float n1, float n2) {
+	return n1 - n2;
+}
+
+float multiplicacao(float n1, float n2) {
+	return n1 * n2;
+}
+
+float divisao(float n1, float n2) {
+	return n1 / n2;
+}
+
+void menu() {
+	while(1) {
+        printf("[1] - Soma \n");
+        printf("[2] - Subtração \n");
+        printf("[3] - Multiplicação \n");
+        printf("[4] - Divisão \n\n");
+
+        printf("Escolha uma opção de cálculo: ");
+        scanf(" %d", &escolha);
+
+        if(escolha < 1 || escolha > 4) {
+            printf("\nOpção inválida! Você precisa digitar corretamente uma das opções informadas. \n");
+        } else {
+            printf("\nDigite o primeiro valor: ");
+            scanf(" %f", &n1);
+            printf("Digite o segundo valor: ");
+            scanf(" %f", &n2);
+
+            switch(escolha) {
+            case 1:
+                resultado = soma(n1, n2);
+                printf("\nResultado: %.2f \n\n", resultado);
+                break;
+            case 2:
+                resultado = subtracao(n1, n2);
+                printf("\nResultado: %.2f \n\n", resultado);
+                break;
+            case 3:
+                resultado = multiplicacao(n1, n2);
+                printf("\nResultado: %.2f \n\n", resultado);
+                break;
+            case 4:
+                resultado = divisao(n1, n2);
+                printf("\nResultado: %.2f \n\n", resultado);
+                break;
+            }
+        }
+
+        system("pause");
+        system("cls");
+    }
+	
+}
+
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+	
+	menu();
+	
+	return 0;
+}
