d00bb14628922c656a8db85b4b36a108edaa6e80
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-03-12 23:58:07 -0300

    Create arrayNumerosRandomicos.c

---
+++ algoritmos/arrayNumerosRandomicos.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+	
+	srand(time(NULL));
+	
+	int array[5][5], i, j;
+	
+	for(i = 0; i < 5; i++) {
+		for(j = 0; j < 5; j++) {
+			array[i][j] = (rand() % 100) + 1;
+			if(array[i][j] < 10) {
+				printf(" ");
+			}
+			printf("%d ", array[i][j]);
+		}
+		printf("\n");
+	}
+}
