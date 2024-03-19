240c81612b648e80a7999f336dd884d605d84daf
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-03-18 21:54:26 -0300

    Update aumentoSalario.c

--- algoritmos/aumentoSalario.c
+++ algoritmos/aumentoSalario.c
@@ -5,18 +5,27 @@
 int main(){
 	setlocale(LC_ALL, "portuguese");
 	
-	float salario, novoSalario;
+	int i, qtdFunc;
+
+	printf("De quantos funcionários você deseja aumentar o salário? ");
+	scanf("%d", &qtdFunc);
+
+	float salario[qtdFunc];
 	
-	printf("Digite o salário do funcionário: ");
-	scanf("%f", &salario);
-	
-	if (salario <= 2000) {
-		novoSalario = salario * 1.30;
-	} else {
-		novoSalario = salario * 1.10;
+	for (i=0; i<qtdFunc; ++i) {
+		printf("Digite o salário do funcionário %d: ", i + 1);
+		scanf(" %d", &salario[i]);
 	}
 	
-	printf("O salário aumentou de R$%.2f para R$%.2f. \n", salario, novoSalario);
+	for (i=0; i<qtdFunc; ++i) {
+		if (salario[i] <= 2000){
+			salario[i] *= 1.30;
+			printf("O salário do funcionario %d aumentou para: R$%d \n", i + 1, salario[i]);
+		} else{
+			salario[i] *= 1.10;
+			printf("O salário do funcionario %d aumentou para: R$%d \n", i + 1, salario[i]);
+		}
+	}
 
 	system("pause");
 	
