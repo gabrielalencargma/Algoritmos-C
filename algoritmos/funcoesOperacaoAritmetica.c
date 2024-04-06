db258b7c065a4e654a64bc238bb7156e37dbca7d
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-04-05 23:29:11 -0300

    Update funcoesOperacaoAritmetica.c

--- algoritmos/funcoesOperacaoAritmetica.c
+++ algoritmos/funcoesOperacaoAritmetica.c
@@ -5,40 +5,48 @@
 int escolha;
 float n1, n2, resultado;
 
+// Função para retornar a soma de n1 e n2
 float soma(float n1, float n2) {
 	return n1 + n2;
 }
 
+// Função para retornar a subtração de n1 e n2
 float subtracao(float n1, float n2) {
 	return n1 - n2;
 }
 
+// Função para retornar a multiplicação de n1 e n2
 float multiplicacao(float n1, float n2) {
 	return n1 * n2;
 }
 
+// Função para retornar a divisão de n1 e n2
 float divisao(float n1, float n2) {
 	return n1 / n2;
 }
 
+// Função do menu do programa
 void menu() {
 	while(1) {
         printf("[1] - Soma \n");
         printf("[2] - Subtração \n");
         printf("[3] - Multiplicação \n");
         printf("[4] - Divisão \n\n");
-
+        
+        // Definindo o valor de escolha
         printf("Escolha uma opção de cálculo: ");
         scanf(" %d", &escolha);
 
         if(escolha < 1 || escolha > 4) {
             printf("\nOpção inválida! Você precisa digitar corretamente uma das opções informadas. \n");
         } else {
+            // Definindo os valores de n1 e n2
             printf("\nDigite o primeiro valor: ");
             scanf(" %f", &n1);
             printf("Digite o segundo valor: ");
             scanf(" %f", &n2);
 
+            // Decidindo a operação aritmética com base no valor de escolha definido pelo usuário
             switch(escolha) {
             case 1:
                 resultado = soma(n1, n2);
