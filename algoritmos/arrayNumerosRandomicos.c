caf772447300997d45f6f897e3454336e6f8512c
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-04-07 23:33:15 -0300

    Update arrayNumerosRandomicos.c

--- algoritmos/arrayNumerosRandomicos.c
+++ algoritmos/arrayNumerosRandomicos.c
@@ -10,9 +10,14 @@ int main() {
 	
 	int array[5][5], i, j;
 	
+	// Estrutura for utilizando "i" para escrever linha por linha.
 	for(i = 0; i < 5; i++) {
+		// Estrutura for utilizando "j" para escrever nas colunas de cada linha.
 		for(j = 0; j < 5; j++) {
+			// Definindo um número randomico para o indice da coluna e linha atual.
 			array[i][j] = (rand() % 100);
+
+			// Print do número, e caso ele seja menor que 10, será adicionado um zero à esquerda.
 			if(array[i][j] < 10) {
 				printf("0%d ", array[i][j]);
 			} else {
