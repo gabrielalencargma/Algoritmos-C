98b28603c4970840004bbf39053e811ea4b9834c
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-05-14 23:40:19 -0300

    Update tabuada.c

--- algoritmos/tabuada.c
+++ algoritmos/tabuada.c
@@ -9,15 +9,16 @@ int main()
 	int n, i;
 	
 	printf("Escolha um número: ");
-	scanf(" %d", &n);
+	scanf(" %d", &n); // "n" recebe o número digitado pelo usuário.
 	
     system("cls");
 
     printf("Tabuada do %d\n\n", n);
 
+	// Estrutura for para contar de 0 a 10.
 	for(i = 0; i <= 10; i++)
 	{
-		printf("%d x %d = %d\n", n, i, n * i);
+		printf("%d x %d = %d\n", n, i, n * i); // Print do valor atual de "i" multiplicado por "n".
 	}
 	
 	printf("\n");
