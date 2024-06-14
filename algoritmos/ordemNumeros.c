723bbcc9a2e823b771b15e58a9d7608b23e2797c
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-06-13 23:19:06 -0300

    Update ordemNumeros.c

--- algoritmos/ordemNumeros.c
+++ algoritmos/ordemNumeros.c
@@ -4,6 +4,7 @@
 
 int i, j, temp, quantidade, n[30];
 
+// Função para printar os números na ordem normal.
 void printNumeros() {
 	for (i = 0; i < quantidade; ++i) {
     	if(i == quantidade - 1) {
@@ -14,9 +15,10 @@ void printNumeros() {
 	}
 }
 
+// Função para ordenar os números em ordem crescente.
 void crescente() {
     printf("Números na ordem de inserção: ");
-	printNumeros();
+	printNumeros(); // Chamando a função "printNumeros()".
 
 	for (i = 0; i < quantidade; ++i) {
 	    for (j = i + 1; j < quantidade; ++j) {
@@ -32,9 +34,10 @@ void crescente() {
 	printNumeros();
 }
 
+// Função para ordenar os números em ordem decrescente.
 void decrescente() {
     printf("Números na ordem de inserção: ");
-	printNumeros();
+	printNumeros(); // Chamando a função "printNumeros()".
 
 	for (i = 0; i < quantidade; ++i) {
 	    for (j = i + 1; j < quantidade; ++j) {
@@ -56,24 +59,26 @@ int main() {
 	int escolha;
 	
 	printf("Digite quantos números serão inseridos: \n");
-	scanf(" %d", &quantidade);
+	scanf(" %d", &quantidade); // "quantidade" recebe a quantidade digitada pelo usuário.
 	
 	printf("\nDigite o(s) número(s): \n");
+	// Estrutura for para contar os índices do vetor "n" até a quantidade informada pelo usuário.
 	for (i = 0; i < quantidade; ++i){
-		scanf(" %d", &n[i]);
+		scanf(" %d", &n[i]); // Definindo o valor do índice atual.
 	}
 	
 	printf("\nComo voce deseja ordená-los? \n[0] - Ordem crescente \n[1] - Ordem decrescente\n\n");
-	scanf(" %d", &escolha);
+	scanf(" %d", &escolha); // "escolha" recebe o número digitado pelo usuário.
 	
 	system("cls");
 	
+	// Verificação do valor de "escolha" para definir qual ordenação dos números realizar.
 	switch(escolha) {
 		case 0:
-			crescente();
+			crescente(); // Chamando a função "crescente()".
 			break;
 		case 1:
-			decrescente();
+			decrescente(); // Chamando a função "decrescente()".
 			break;
 		default:
 			printf("Opção inválida! Finalizando programa...");
