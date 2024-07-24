c958fe87c546cedf6d42cae080d4c06c4b7a995b
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-07-23 22:58:12 -0300

    Update checagemDesconto.c

--- algoritmos/checagemDesconto.c
+++ algoritmos/checagemDesconto.c
@@ -4,21 +4,21 @@
 
 float total;
 
-// Função para aplicar desconto de funcionário
+// Função para aplicar desconto de funcionário.
 void desconto_funcionario(float valor) {
-	float total = valor - (valor * 0.10); // "total" recebe 10% de desconto aplicado em "valor"
+	float total = valor - (valor * 0.10); // "total" recebe os 10% de desconto aplicado em "valor".
 	printf("\nValor do desconto: R$%.2f \n", valor*0.10);
 	printf("Total à pagar: R$%.2f \n", total);
 }
 
-// Função para aplicar desconto de cliente vip
+// Função para aplicar desconto de cliente vip.
 void desconto_vip(float valor) {
-	float total = valor - (valor * 0.05); // "total" recebe 5% de desconto aplicado em "valor"
+	float total = valor - (valor * 0.05); // "total" recebe os 5% de desconto aplicado em "valor".
 	printf("\nValor do desconto: R$%.2f \n", valor*0.05);
 	printf("Total à pagar: R$%.2f \n", total);
 }
 
-// Menu do programa
+// Menu do programa.
 void menu() {
 	int id;
 	float valor;
@@ -33,17 +33,17 @@ void menu() {
 	
 	scanf("%d", &id); // "id" recebe o número digitado pelo usuário.
 
-	// Verificação do valor de "id" para definir o desconto da compra.
+	// Verificação do valor de "id" para escolher o desconto da compra.
 	switch(id) {
 		case 1:
 			printf("\nSem desconto! \n"); // Não há desconto para cliente comum.
 			printf("Total à pagar: R$%.2f \n", valor);
 			break;
 		case 2:
-			desconto_funcionario(valor); // Chamando a função de desconto para funcionário.
+			desconto_funcionario(valor); // Chamando a função de desconto para funcionário, usando "valor" como argumento.
 			break;
 		case 3:
-			desconto_vip(valor); // Chamando a função de desconto para cliente vip.
+			desconto_vip(valor); // Chamando a função de desconto para cliente vip, usando "valor" como argumento.
 			break;
 		default:
 			printf("\nNuméro de identificação inválido! \n");
