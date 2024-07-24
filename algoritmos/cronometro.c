a96c69c7171bdf779e4fcd4470ddfc34d18955f0
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-07-23 23:24:38 -0300

    Update cronometro.c

--- algoritmos/cronometro.c
+++ algoritmos/cronometro.c
@@ -8,23 +8,22 @@ int main() {
 	
 	int horas = 0, minutos = 0, segundos = 0;
     
-	// Estrutura while para fazer a cronometragem em segundos.
+	// Estrutura while cronometrar o tempo.
 	while(1) {
 		system("cls");
-		segundos++;
-		// Quando a varíavel "segundos" chegar a 60, seu valor será zerado, e a varíavel "minutos" aumentará em 1.
+		segundos++; // Incrementando 1 no valor de "segundos".
+		// Se o valor de "segundos" chegar a 60, ele será zerado e o valor de "minutos" incrementará 1.
 		if(segundos == 60) {
 			segundos = 0;
 			minutos++;
-			// Quando a varíavel "minutos" chegar a 60, seu valor será zerado, e a varíavel "horas" aumentará em 1.
+			// Se o valor de "minutos" chegar a 60, ele será zerado e o valor de "horas" incrementará 1.
 			if(minutos == 60) {
 				minutos = 0;
 				horas++;
 			}
 		}
 		printf("%.2d : %.2d : %.2d", horas, minutos, segundos);
-		// Intervalo de 1 segundo antes da repetição do processo.
-		sleep(1);
+		sleep(1); // Pausa de 1 segundo.
 	}
 	
 	return 0;
