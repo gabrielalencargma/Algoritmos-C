f73842d7f644b6bc9f9c03559ddfe0876ea95ff9
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2024-07-24 23:48:16 -0300

    Update operacoesAritmeticas.c

--- algoritmos/operacoesAritmeticas.c
+++ algoritmos/operacoesAritmeticas.c
@@ -25,43 +25,42 @@ float divisao(float n1, float n2) {
 	return n1 / n2;
 }
 
-// Função para exibir o menu do programa.
+// Menu do programa.
 void menu() {
+    // Estrutura while para repetir a escolha das opções.
 	while(1) {
         printf("[1] - Soma \n");
         printf("[2] - Subtração \n");
         printf("[3] - Multiplicação \n");
         printf("[4] - Divisão \n\n");
         
-        // Definindo o valor de "escolha".
         printf("Escolha uma opção de cálculo: ");
-        scanf(" %d", &escolha);
+        scanf(" %d", &escolha); // "escolha" recebe o número digitado pelo usuário.
 
         if(escolha < 1 || escolha > 4) {
             printf("\nOpção inválida! Você precisa digitar corretamente uma das opções informadas. \n");
         } else {
-            // Definindo os valores de "n1" e "n2".
             printf("\nDigite o primeiro valor: ");
-            scanf(" %f", &n1);
+            scanf(" %f", &n1); // "n1" recebe o valor digitado pelo usuário.
             printf("Digite o segundo valor: ");
-            scanf(" %f", &n2);
+            scanf(" %f", &n2); // "n2" recebe o valor digitado pelo usuário.
 
-            // Verificação do valor de "escolha" para definir o tipo de cálculo.
+            // Verificação do valor de "escolha" para escolher a operação.
             switch(escolha) {
             case 1:
-                resultado = soma(n1, n2); // "resultado" recebe o retorno da função "soma" com os parâmetros "n1" e "n2".
+                resultado = soma(n1, n2); // "resultado" recebe o retorno da função "soma()" com os atributos "n1" e "n2".
                 printf("\nResultado: %.2f \n\n", resultado);
                 break;
             case 2:
-                resultado = subtracao(n1, n2); // "resultado" recebe o retorno da função "subtracao" com os parâmetros "n1" e "n2".
+                resultado = subtracao(n1, n2); // "resultado" recebe o retorno da função "subtracao()" com os atributos "n1" e "n2".
                 printf("\nResultado: %.2f \n\n", resultado);
                 break;
             case 3:
-                resultado = multiplicacao(n1, n2); // "resultado" recebe o retorno da função "multiplicacao" com os parâmetros "n1" e "n2".
+                resultado = multiplicacao(n1, n2); // "resultado" recebe o retorno da função "multiplicacao()" com os atributos "n1" e "n2".
                 printf("\nResultado: %.2f \n\n", resultado);
                 break;
             case 4:
-                resultado = divisao(n1, n2); // "resultado" recebe o retorno da função "divisao" com os parâmetros "n1" e "n2".
+                resultado = divisao(n1, n2); // "resultado" recebe o retorno da função "divisao()" com os atributos "n1" e "n2".
                 printf("\nResultado: %.2f \n\n", resultado);
                 break;
             }
@@ -76,7 +75,7 @@ void menu() {
 int main() {
 	setlocale(LC_ALL, "Portuguese");
 	
-	menu();
+	menu(); // Chamando a função de menu do programa.
 	
 	return 0;
 }
